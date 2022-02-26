4e5111aa401e12f0a21197e8d4e46d37940ef247
ПК <@->
2022-02-26 14:19:34 +0300

    Добавьте файлы проекта.

---
+++ ExcelMacroAdd/Ribbon1.Designer.cs
@@ -0,0 +1,115 @@
+﻿namespace ExcelMacroAdd
+{
+    partial class Ribbon1 : Microsoft.Office.Tools.Ribbon.RibbonBase
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        public Ribbon1()
+            : base(Globals.Factory.GetRibbonFactory())
+        {
+            InitializeComponent();
+        }
+
+        /// <summary> 
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором компонентов
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Ribbon1));
+            this.tab1 = this.Factory.CreateRibbonTab();
+            this.Группа1 = this.Factory.CreateRibbonGroup();
+            this.button1 = this.Factory.CreateRibbonButton();
+            this.button2 = this.Factory.CreateRibbonButton();
+            this.button3 = this.Factory.CreateRibbonButton();
+            this.tab1.SuspendLayout();
+            this.Группа1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tab1
+            // 
+            this.tab1.Groups.Add(this.Группа1);
+            this.tab1.Label = "МАКРОСЫ";
+            this.tab1.Name = "tab1";
+            // 
+            // Группа1
+            // 
+            this.Группа1.Items.Add(this.button1);
+            this.Группа1.Items.Add(this.button2);
+            this.Группа1.Items.Add(this.button3);
+            this.Группа1.Label = "Базовые макросы";
+            this.Группа1.Name = "Группа1";
+            // 
+            // button1
+            // 
+            this.button1.Image = ((System.Drawing.Image)(resources.GetObject("button1.Image")));
+            this.button1.Label = "Удалить формулы";
+            this.button1.Name = "button1";
+            this.button1.ShowImage = true;
+            this.button1.Click += new Microsoft.Office.Tools.Ribbon.RibbonControlEventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Image = ((System.Drawing.Image)(resources.GetObject("button2.Image")));
+            this.button2.Label = "Разметка листов";
+            this.button2.Name = "button2";
+            this.button2.ShowImage = true;
+            this.button2.Click += new Microsoft.Office.Tools.Ribbon.RibbonControlEventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Image = ((System.Drawing.Image)(resources.GetObject("button3.Image")));
+            this.button3.Label = "Корпуса щитов";
+            this.button3.Name = "button3";
+            this.button3.ShowImage = true;
+            this.button3.Click += new Microsoft.Office.Tools.Ribbon.RibbonControlEventHandler(this.button3_Click);
+            // 
+            // Ribbon1
+            // 
+            this.Name = "Ribbon1";
+            this.RibbonType = "Microsoft.Excel.Workbook";
+            this.Tabs.Add(this.tab1);
+            this.Load += new Microsoft.Office.Tools.Ribbon.RibbonUIEventHandler(this.Ribbon1_Load);
+            this.tab1.ResumeLayout(false);
+            this.tab1.PerformLayout();
+            this.Группа1.ResumeLayout(false);
+            this.Группа1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        internal Microsoft.Office.Tools.Ribbon.RibbonTab tab1;
+        internal Microsoft.Office.Tools.Ribbon.RibbonGroup Группа1;
+        internal Microsoft.Office.Tools.Ribbon.RibbonButton button1;
+        internal Microsoft.Office.Tools.Ribbon.RibbonButton button2;
+        internal Microsoft.Office.Tools.Ribbon.RibbonButton button3;
+    }
+
+    partial class ThisRibbonCollection
+    {
+        internal Ribbon1 Ribbon1
+        {
+            get { return this.GetRibbon<Ribbon1>(); }
+        }
+    }
+}
---
+++ ExcelMacroAdd/Ribbon1.cs
@@ -0,0 +1,114 @@
+﻿using Microsoft.Office.Tools.Ribbon;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Windows.Forms;
+using Excel = Microsoft.Office.Interop.Excel;
+
+namespace ExcelMacroAdd
+{
+    public partial class Ribbon1
+    {
+            private void Ribbon1_Load(object sender, RibbonUIEventArgs e)
+        {
+
+        }
+        
+        private void button1_Click(object sender, RibbonControlEventArgs e) //Удаление формул
+        {
+            Excel.Application application = (Excel.Application)Marshal.GetActiveObject("Excel.Application");
+            Excel.Worksheet worksheet = ((Excel.Worksheet)application.ActiveSheet);
+            Excel.Range cell = application.Selection;
+            cell.Value = cell.Value;                    //Удаляем формулы
+            worksheet.get_Range("A1", "A1").Select();   //Фокус на ячейку А1
+        }
+
+        private void button2_Click(object sender, RibbonControlEventArgs e) //Разметка шаблона расчетов
+        {
+            Excel.Application application = (Excel.Application)Marshal.GetActiveObject("Excel.Application");
+            Excel.Worksheet worksheet = ((Excel.Worksheet)application.ActiveSheet);
+            Excel.Range cell = application.Selection;
+
+            //состовляем надписи колонок           
+            worksheet.get_Range("A1").Value2 = "Артикул";
+            worksheet.get_Range("B1").Value2 = "Описание";
+            worksheet.get_Range("C1").Value2 = "Кол-во";
+            worksheet.get_Range("D1").Value2 = "Кратность";
+            worksheet.get_Range("E1").Value2 = "Пр-ль";
+            worksheet.get_Range("F1").Value2 = "Скидка";
+            worksheet.get_Range("G1").Value2 = "Цена";
+            worksheet.get_Range("H1").Value2 = "Цена со скидкой";
+            worksheet.get_Range("I1").Value2 = "Стоимость";
+
+            //увеличиваем размер по ширине диапазон ячеек
+            worksheet.get_Range("A1", "A1").EntireColumn.ColumnWidth = 21;
+            worksheet.get_Range("B1", "B1").EntireColumn.ColumnWidth = 80;
+            worksheet.get_Range("C1", "C1").EntireColumn.ColumnWidth = 10;
+            worksheet.get_Range("D1", "I1").EntireColumn.ColumnWidth = 13;
+
+            //размечаем границы и правим шрифты
+            worksheet.get_Range("A1", "i500").Cells.Font.Name = "Calibri";
+            worksheet.get_Range("A1", "i500").Cells.Font.Size = 11;
+
+            var Excelcells = worksheet.get_Range("A1", "I11");
+            Excel.XlBordersIndex BorderIndex;
+
+            BorderIndex = Excel.XlBordersIndex.xlEdgeLeft; //Левая граница
+            Excelcells.Borders[BorderIndex].Weight = Excel.XlBorderWeight.xlThin;
+            Excelcells.Borders[BorderIndex].LineStyle = Excel.XlLineStyle.xlContinuous;
+            Excelcells.Borders[BorderIndex].ColorIndex = 0;
+
+            BorderIndex = Excel.XlBordersIndex.xlEdgeTop; //Верхняя граница
+            Excelcells.Borders[BorderIndex].Weight = Excel.XlBorderWeight.xlThin;
+            Excelcells.Borders[BorderIndex].LineStyle = Excel.XlLineStyle.xlContinuous;
+            Excelcells.Borders[BorderIndex].ColorIndex = 0;
+
+            BorderIndex = Excel.XlBordersIndex.xlEdgeBottom; //Нижняя граница
+            Excelcells.Borders[BorderIndex].Weight = Excel.XlBorderWeight.xlThin;
+            Excelcells.Borders[BorderIndex].LineStyle = Excel.XlLineStyle.xlContinuous;
+            Excelcells.Borders[BorderIndex].ColorIndex = 0;
+
+            BorderIndex = Excel.XlBordersIndex.xlEdgeRight;  //Правая граница
+            Excelcells.Borders[BorderIndex].Weight = Excel.XlBorderWeight.xlThin;
+            Excelcells.Borders[BorderIndex].LineStyle = Excel.XlLineStyle.xlContinuous;
+            Excelcells.Borders[BorderIndex].ColorIndex = 0;
+
+            BorderIndex = Excel.XlBordersIndex.xlInsideHorizontal;  //Внутренняя горизонтальня граница
+            Excelcells.Borders[BorderIndex].Weight = Excel.XlBorderWeight.xlThin;
+            Excelcells.Borders[BorderIndex].LineStyle = Excel.XlLineStyle.xlContinuous;
+            Excelcells.Borders[BorderIndex].ColorIndex = 0;
+
+            BorderIndex = Excel.XlBordersIndex.xlInsideVertical;  //Внутренняя горизонтальня граница
+            Excelcells.Borders[BorderIndex].Weight = Excel.XlBorderWeight.xlThin;
+            Excelcells.Borders[BorderIndex].LineStyle = Excel.XlLineStyle.xlContinuous;
+            Excelcells.Borders[BorderIndex].ColorIndex = 0;
+        }
+
+        private void button3_Click(object sender, RibbonControlEventArgs e) //Корпуса щитов
+        {
+            Excel.Application application = (Excel.Application)Marshal.GetActiveObject("Excel.Application");
+            Excel.Worksheet worksheet = ((Excel.Worksheet)application.ActiveSheet);
+            Excel.Range cell = application.Selection;
+
+            if  (application.ActiveWorkbook.Name == "_Журнал учета НКУ 2022.xlsx") {
+                
+            
+            
+            
+            
+            }
+            else
+            {
+                MessageBox.Show(
+                "Программа работает только в файле _Журнал учета НКУ 2022.xlsx \n Пожайлуста откройте целевую книгу и запустите программу.",
+                "Ошибка вызова",
+                MessageBoxButtons.OK,
+                MessageBoxIcon.Information,
+                MessageBoxDefaultButton.Button1,
+                MessageBoxOptions.DefaultDesktopOnly);
+            }
+        }
+    }
+}
