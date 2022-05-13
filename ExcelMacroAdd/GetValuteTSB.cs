0ee8e9e143c2a4934d44ac4134443c1f1a325fc3
Vladislav <vlad1988_1@mail.ru>
2022-05-13 18:06:54 +0300

    Исправление ошибки слияния

--- ExcelMacroAdd/GetValuteTSB.cs
+++ ExcelMacroAdd/GetValuteTSB.cs
@@ -20,7 +20,7 @@ namespace ExcelMacroAdd
                 foreach (DataRow row in currency.Rows)
                 {
                     //Поиск доллара
-                    if (row["NumCode"].ToString() == "IEK") USDRate = Math.Round(Convert.ToDouble(row["Value"]), 2);
+                    if (row["CharCode"].ToString() == "USD") USDRate = Math.Round(Convert.ToDouble(row["Value"]), 2);
                   
                     // Поиск ЕВРО
                     if (row["CharCode"].ToString() == "EUR") EvroRate = Math.Round(Convert.ToDouble(row["Value"]), 2);
