d614f7cf57f99b9954c73a269e413dd1ff49130c
Vladislav <vlad1988_1@mail.ru>
2022-12-02 08:50:07 +0300

    Фиксация версии приложения

--- ExcelMacroAdd/Properties/AssemblyInfo.cs
+++ ExcelMacroAdd/Properties/AssemblyInfo.cs
@@ -32,6 +32,6 @@ using System.Runtime.InteropServices;
 // Можно задать все значения или принять номера сборки и редакции по умолчанию 
 // используя "*", как показано ниже:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.7.4.5")]
-[assembly: AssemblyFileVersion("1.7.4.5")]
+[assembly: AssemblyVersion("1.7.4.6")]
+[assembly: AssemblyFileVersion("1.7.4.6")]
 [assembly: NeutralResourcesLanguage("")]
